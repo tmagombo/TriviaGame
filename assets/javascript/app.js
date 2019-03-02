ac71fd1ba98defa936b5b9d6fa0e77f3c3cd97d5
Tristan <magombotristan@gmail.com>
2019-03-01 20:58:09 -0700

    Got the timer to work as well as a diff flag to display every 20 sec

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,32 +5,56 @@ var questions=["assets/images/Argentina.png","assets/images/Algeria.png",
 "assets/images/Syria.png","assets/images/Thailand.png"];
 var qCorrect = 0;
 var qIncorrect = 0;
+var time = 4;
 var count = 0;
-
+var intervalId;
+console.log(questions.length);
 
 function questionTimer(){
-if(count==0){
-    displayImage();
-    count++;
+if(count===questions.length){
+count=0;
+questionTimer();
 }
-else if(count>questions.length-1){
-    count=0;
-    questionTimer();
-
-}
-
 else{ 
-displayImage();
-count++;
-console.log(questions[count]);
+displayQuestion();
+console.log(count);
 }
 };
 
 function startGame(){
-setInterval(questionTimer, 1000);
+count=0;
+setInterval(questionTimer, 20000);
+displayQuestion();
+
+};
+intervalId = setInterval(startTimer, 1000);
+
+
+function startTimer(){
+    time--;
+    $("#timer").text(time + " seconds left");
+    if(time===0){
+        stopTimer();
+    }
 };
 
-function displayImage() {
+function stopTimer(){
+    clearInterval(intervalId);
+    qIncorrect();
+};
+
+function displayQuestion() {
     $("#question").html("<img src=" + questions[count] + ">");
-  }
+    startTimer();
+    count++;
+    
+  };
+
+  function qIncorrect(){
+      $("#question").empty();
+      qIncorrect++;
+  };
+
+
+
 startGame();
